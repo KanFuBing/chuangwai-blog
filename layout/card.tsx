7ab4ccf7af1a0704552d965ce57cf290509a95d8
KanFuBing <110780208+KanFuBing@users.noreply.github.com>
2022-08-27 13:49:46 +0800

    修改 Twitter Card 图标

--- layout/card.tsx
+++ layout/card.tsx
@@ -10,7 +10,7 @@ const LayoutTwitterCard = ({ title }: LayoutTwitterCardProps) => (
         <meta name='twitter:site' content='@KanFuBing' />
         <meta name='twitter:title' content={title} />
         <meta name='twitter:description' content='你好，一块浮冰上的博客。' />
-        <meta name='twitter:image' content='' />
+        <meta name='twitter:image' content='https://firebasestorage.googleapis.com/v0/b/chuang-wai.appspot.com/o/android-chrome-192x192.png?alt=media&token=8c579060-f410-4f25-ba87-5aefcb8b536e' />
     </Head>
 )
 
