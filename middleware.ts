4847245ae5bb89486b7487ec79e8eeacc605e361
KanFuBing <110780208+KanFuBing@users.noreply.github.com>
2022-08-27 20:06:40 +0800

    取消阻挡 403 图片

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,9 @@ const BLOCKED_COUNTRY = 'CN'
 
 export function middleware(req: NextRequest) {
   const country = (req.geo && req.geo.country) || 'US'
-  if (country === BLOCKED_COUNTRY) {
+  const isRequestingStatusImage = req.nextUrl.pathname.startsWith('/status')
+  // 不应阻挡对 403 状态图片的大陆请求
+  if (!isRequestingStatusImage && country === BLOCKED_COUNTRY) {
     req.nextUrl.pathname = '/403'
   }
   return NextResponse.rewrite(req.nextUrl)
